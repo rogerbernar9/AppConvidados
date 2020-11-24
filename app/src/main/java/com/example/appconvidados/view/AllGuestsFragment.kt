29847a35e0b726a54fbbf30805ce3cb976b2c999
rogerbernar9 <roger.bernar9@gmail.com>
2020-11-24 15:52:38 -0300

    aula 160 listagem de todos os convidados parte 1

--- app/src/main/java/com/example/appconvidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/appconvidados/view/AllGuestsFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appconvidados.R
+import com.example.appconvidados.view.adapter.GuestAdapter
 import com.example.appconvidados.viewmodel.AllGuestsViewModel
 
 class AllGuestsFragment : Fragment() {
@@ -22,11 +26,18 @@ class AllGuestsFragment : Fragment() {
     ): View? {
         allGuestsViewModel =
             ViewModelProviders.of(this).get(AllGuestsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        allGuestsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val root = inflater.inflate(R.layout.fragment_all, container, false)
+
+        // RecyclerView
+        // obter recycler
+        val recycler = root.findViewById<RecyclerView>(R.id.recycler_all)
+
+        // definir layout
+        recycler.layoutManager = LinearLayoutManager(context)
+
+        // definir adapter
+        recycler.adapter = GuestAdapter()
+
         return root
     }
 }
---
+++ app/src/main/java/com/example/appconvidados/view/adapter/GuestAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.appconvidados.view.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appconvidados.view.viewholder.GuestViewHolder
+
+class GuestAdapter : RecyclerView.Adapter<GuestViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GuestViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: GuestViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
